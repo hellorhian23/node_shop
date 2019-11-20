9ccd4a1f71489e04370be95d004b607310038c11
Rhian <hellorhian23@gmail.com>
2019-11-20 19:01:21 +0900

    product posting

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+const productModel = require("../models/products");
+
+
 // 데이터 불러오기(products)
 router.get("/", (req, res) => {
     res.json({
@@ -13,15 +16,38 @@ router.get("/", (req, res) => {
 // 데이터 등록하기 (product)
 router.post("/", (req, res) => {
 
-    const product = {
-        name : req.body.productName,
+    const product = new productModel({
+        name: req.body.productName,
         price : req.body.productPrice
-    };
-
-    res.json({
-        message : "successful product post",
-        productInfo : product
     });
+
+    product
+        .save()
+        .then(doc =>{
+            res.json({
+                message : "registered product",
+                productInfo : doc
+            });
+        })
+        .catch(err => {
+            res.json({
+                // 메세지 문구 설정할 경우 "" 명령어 설정
+                // 하단의 명령어는 err 임을 보여줌
+                msg : err.message
+            });
+        });
+
+
+   //command + / (하고자 하는 영역) : 주석처리 (컴퓨터가 읽지 못함, build 안함)
+    // const product = {
+    //     name : req.body.productName,
+    //     price : req.body.productPrice
+    // };
+
+    // res.json({
+    //     message : "successful product post",
+    //     productInfo : product
+    // });
 
 });
 
