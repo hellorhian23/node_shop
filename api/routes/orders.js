012c7ac6d0f447acd212796b75bb942542c4b400
Rhian <hellorhian23@gmail.com>
2019-11-20 20:33:11 +0900

    orders total get and post

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,28 +2,67 @@ const express = require("express");
 
 const router = express.Router();
 
+const orderModel = require("../models/orders");
+
 //장바구니 불러오기
 router.get("/", (req, res) => {
-    res.json({
-        message : "Hellorhian"
-
+   
+   orderModel
+    .find()
+    .exec()
+    .then(docs => {
+        res.json({
+            message : "successful orders get",
+            count : docs.length,
+            orders : docs
+        });
+    })
+    .catch(err => {
+        res.json({
+                message : err.message
+        });
     });
+
+
+    // res.json({
+    //     message : "Hellorhian"
+
+    // });
 });
 
 //장바구니 등록하기
 router.post("/", (req, res) => {
 
-    const order ={
-        name : req.body.productName,
+    const order = new orderModel({
+        product : req.body.product_ID,
         qty : req.body.qty
-    };
-
-
-    res.json({
-    
-        message : "successful add",
-        orderInfo : order
     });
+
+    order
+        .save()
+        .then(result => {
+            res.json({
+                message : "successful order stored",
+                orderInfo : result
+            });
+        })
+        .catch(err => {
+            res.json({
+                message : err.message
+            });
+        });
+
+    // const order ={
+    //     name : req.body.productName,
+    //     qty : req.body.qty
+    // };
+
+
+    // res.json({
+    
+    //     message : "successful add",
+    //     orderInfo : order
+    // });
 });
 
 //장바구니 삭제하기
